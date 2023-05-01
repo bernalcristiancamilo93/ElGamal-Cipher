c4d664c510a92f8025e18f1b40686143afed5178
Camilo Bernal <bernalcristiancamilo93@gmail.com>
2023-05-01 11:13:31 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ElGamal-Cipher',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
