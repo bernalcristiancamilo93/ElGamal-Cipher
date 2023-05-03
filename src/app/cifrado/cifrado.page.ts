ac870735e3d5f9c1ba67779c7acad4f1a05b66ff
Camilo Bernal <bernalcristiancamilo93@gmail.com>
2023-05-03 09:37:32 -0500

    Elgamal cipher is finished

--- src/app/cifrado/cifrado.page.ts
+++ src/app/cifrado/cifrado.page.ts
@@ -10,15 +10,15 @@ import { AlertController } from '@ionic/angular';
 export class CifradoPage {
   // Forms
   public claveForm = new FormGroup({
-    primoP: new FormControl(107, Validators.required),
-    generadorG: new FormControl(32, Validators.required),
-    numeroK: new FormControl(49, Validators.required),
+    primoP: new FormControl('', Validators.required),
+    generadorG: new FormControl('', Validators.required),
+    numeroK: new FormControl('', Validators.required),
     numLetrasPorBloque: new FormControl(2, Validators.required),
   });
 
   public cifradoForm: FormGroup = new FormGroup({
-    textoClaroInput: new FormControl('qwerty', Validators.required),
-    aleatorioX: new FormControl(74, Validators.required),
+    textoClaroInput: new FormControl('', Validators.required),
+    aleatorioX: new FormControl('', Validators.required),
     textoClaroFormateado: new FormControl(),
     pesosTextoClaroFormateado: new FormControl(),
     pesosCifradosOutput: new FormControl(),
@@ -31,6 +31,37 @@ export class CifradoPage {
 
   // Flags
   public sonParametrosCorrectos = false;
+
+  // Alfabeto
+  private alfabeto: string[] = [
+    ' ',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
 
   constructor(private alertCtrl: AlertController) {}
 
@@ -63,39 +94,144 @@ export class CifradoPage {
   }
 
   cifrar() {
-    // Transforma el texto claro en pesos
     const numLetrasPorBloque: number =
       +this.claveForm.get('numLetrasPorBloque')?.value!;
 
+    // Quita los acentos, signos de puntuación y pasa todo a mayúsculas y
+    // separa el mensaje en caracteres individuales.
     const textoFragmentado: string[] = this.cifradoForm
       .get('textoClaroInput')
-      ?.value.split('');
+      ?.value.toUpperCase()
+      // .replace(/\s/g, '')
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()!¡¿?]/g, '')
+      .split('');
 
-    console.log(textoFragmentado);
+    // Asigna peso a cada caracter
+    const mensajeConPesos: number[] = [];
 
-    // Calcular el cifrado para peso
-    // Concatenar los pesos cifrados
-    // mostrar los pesos cifrados en pantalla
+    for (const letra of textoFragmentado) {
+      mensajeConPesos.push(this.asignarPeso(letra));
+    }
 
-    const generadorG: number = +this.claveForm.get('generadorG')?.value!;
+    // Verifica los pesos y forma un nuevo array
     const primoP: number = +this.claveForm.get('primoP')?.value!;
+
+    const textoFormateado: string[] = [];
+    const pesosConcatenados: number[] = [];
+
+    for (let i = 0; i < textoFragmentado.length; i++) {
+      let valorConcatenado: number | string = '';
+      valorConcatenado += mensajeConPesos[i];
+      valorConcatenado += mensajeConPesos[i + 1];
+      valorConcatenado = +valorConcatenado;
+
+      if (valorConcatenado < primoP) {
+        textoFormateado.push(textoFragmentado[i] + textoFragmentado[i + 1]);
+        pesosConcatenados.push(valorConcatenado);
+        i++;
+      } else {
+        textoFormateado.push(`  ` + textoFragmentado[i]);
+        pesosConcatenados.push(mensajeConPesos[i]);
+      }
+    }
+
+    // Hace el cifrado para cada peso
+    const generadorG: number = +this.claveForm.get('generadorG')?.value!;
     const aleatorioX: number = +this.cifradoForm.get('aleatorioX')?.value!;
     const numeroK: number = +this.claveForm.get('numeroK')?.value!;
-    const mensaje: number = +this.cifradoForm.get('textoClaroInput')?.value!;
 
     const numeroY = this.calculoExponenteModulo(generadorG, aleatorioX, primoP);
     const numeroR = this.calculoExponenteModulo(numeroY, numeroK, primoP);
-    const numeroS = (mensaje * numeroR) % primoP;
+    const pesosCifrados: number[][] = [];
 
-    console.log('generadorG: ', generadorG);
-    console.log('aleatorioX: ', aleatorioX);
-    console.log('primoP', primoP);
-    console.log('numeroY', numeroY);
-    console.log('numeroR', numeroR);
-    console.log('numeroS', numeroS);
+    for (const peso of pesosConcatenados) {
+      pesosCifrados.push(this.cifrarPeso(peso, numeroR, primoP));
+    }
+
+    // Muestra en pantalla el texto formateado y los pesos correspondientes
+    this.cifradoForm.get('textoClaroFormateado')?.patchValue(textoFormateado);
+    this.cifradoForm
+      .get('pesosTextoClaroFormateado')
+      ?.patchValue(pesosConcatenados);
+
+    let pesosCifradosFormateado: string = '';
+
+    for (const peso of pesosCifrados) {
+      pesosCifradosFormateado += `(${peso[0]}, ${peso[1]});`;
+    }
+
+    pesosCifradosFormateado = pesosCifradosFormateado.substring(
+      0,
+      pesosCifradosFormateado.length - 1
+    );
+
+    this.cifradoForm
+      .get('pesosCifradosOutput')
+      ?.patchValue(pesosCifradosFormateado);
+
+    // Logs de consola
+    // console.log('textoFragmentado', textoFragmentado);
+    // console.log('mensajeConPesos', mensajeConPesos);
+    // console.log('textoFormateado', textoFormateado);
+    // console.log('pesosConcatenados', pesosConcatenados);
   }
 
-  descifrar() {}
+  cifrarPeso(peso: number, numeroR: number, primoP: number): number[] {
+    const numeroS = (peso * numeroR) % primoP;
+
+    // Logs de consola
+    // console.log('generadorG: ', generadorG);
+    // console.log('aleatorioX: ', aleatorioX);
+    // console.log('primoP', primoP);
+    // console.log('numeroY', numeroY);
+    // console.log('numeroR', numeroR);
+    // console.log('numeroS', numeroS);
+
+    return [numeroR, numeroS];
+  }
+
+  descifrar() {
+    const textoCifradoCrudo: string[] = this.descifradoForm
+      .get('textoCifradoInput')
+      ?.value.split(';');
+
+    const parejasPesosCifrados: number[][] = [];
+
+    for (const pareja of textoCifradoCrudo) {
+      parejasPesosCifrados.push(
+        pareja
+          .replace(/[()]/g, '')
+          .split(',')
+          .map((array) => +array)
+      );
+    }
+
+    // Cálculo de clave r inversa
+    const primoP: number = +this.claveForm.get('primoP')?.value!;
+    const numeroR: number = parejasPesosCifrados[0][0];
+
+    let rIversa = 0;
+    for (let i = 1; i < primoP; i++) {
+      if ((i * primoP + 1) % numeroR === 0) {
+        rIversa = (i * primoP + 1) / numeroR;
+        break;
+      }
+    }
+
+    // Descrifado de cada mensaje
+    const pesosDescifados: number[] = [];
+    for (const pareja of parejasPesosCifrados) {
+      pesosDescifados.push((pareja[1] * rIversa) % primoP);
+    }
+
+    this.descifradoForm.get('textoClaroOutput')?.patchValue(pesosDescifados);
+  }
+
+  asignarPeso(letra: string): number {
+    return this.alfabeto.findIndex((caracter) => caracter === letra);
+  }
 
   calculoExponenteModulo(
     base: number,
